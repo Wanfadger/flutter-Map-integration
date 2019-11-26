00999dfda72af9b230e332885632d028cd82600b
Galiwango Fahad <galiwango.fahad@gmail.com>
2019-11-26 16:52:20 +0300

    added PlacesSearchMap

---
+++ lib/places_search_map.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class PlacesSearchMap extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => return _PlacesSearchMap();
+}
+
+class _PlacesSearchMap extends State<PlacesSearchMap> {
+  static const _API_KEY = "AIzaSyB34KgbVhmWCLKsoUXPUXqeJkISvcoveTc";
+  List<Marker> markes = <Marker>[];
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
